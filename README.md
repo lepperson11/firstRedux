b179aa18649324e161ddc7b334c5ef26c7eb5395
lepperson11 <lepperson11@gmail.com>
2024-06-26 15:15:03 -0600

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+I have changed the look of the Counter and buttons, the addition button will add 20 while the subtraction button will subtract 4.
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
@@ -68,3 +70,4 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+# firstRedux
--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./actions";
+import "./index.css";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const isLogged = useSelector((state) => state.isLogged);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="counter">Counter : {counter}</h1>
+      <div className="btns">
+        <button className="plus" onClick={() => dispatch(increment(20))}>
+          +
+        </button>
+        <button className="minus" onClick={() => dispatch(decrement(4))}>
+          -
+        </button>
+      </div>
+      {isLogged ? <h3>Valuable Information I shouldn't see</h3> : ""}
     </div>
   );
 }
